7704f2582911baf65ba8d776449b34b1c24eb96a
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-02 06:38:15 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,15 @@
 class Patient
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @appointments = []
+  end
+
+  def self.all
+    @@all
   end
 
   def appointments
